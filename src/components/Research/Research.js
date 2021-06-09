cc4449ba4e844ffb5f685873b8e38abd1872eec9
Brian Hu <blp.hu5@gmail.com>
2021-06-09 14:48:44 -0400

    Updated research section design, added icons

--- src/components/Research/Research.js
+++ src/components/Research/Research.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { ResearchItems } from "./ResearchItems.js";
 import "./Research.css";
 
-import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 
@@ -15,28 +14,21 @@ class Research extends Component {
             <h1 className="jumbo-title">RESEARCH</h1>
           </Container>
         </Jumbotron>
-
         <div className="content-sidebar">
           {ResearchItems.map((item, index) => {
             return (
               <React.Fragment>
                 <div className="col" id={item.id}>
-                  {/*<span className="title">{item.title}</span>*/}
-                  <Card border="dark" className="research-card-wrapper">
-                    <Card.Body className="research-card-content">
-                      <Card.Title className="research-card-title">
-                        {item.title}
-                      </Card.Title>
-                    </Card.Body>
-                    <Card.Img
-                      className="research-card-image"
-                      variant="top"
-                      src={item.image_url}
-                    />
-                  </Card>
+                  <span className="title">{item.title}</span>
                 </div>
                 <div className="col">
+                  <span className="list-subtitle">
+                    <i className={item.icon1} /> {item.subtitle1}
+                  </span>
                   <span className="list-description">{item.description}</span>
+                  <span className="list-subtitle">
+                    <i className={item.icon2} /> {item.subtitle2}
+                  </span>
                   <span className="list-direction">{item.direction}</span>
                   {/*<div className="list-keywords">
                     {item.keywords.map((word, index) => {
--- src/components/Research/ResearchItems.js
+++ src/components/Research/ResearchItems.js
@@ -1,16 +1,24 @@
 export const ResearchItems = [
+  {},
   {
     title: "Storage Systems for Data Science",
+    subtitle1: "Context",
+    subtitle2: "Research Direction",
+    icon1: "fas fa-globe",
+    icon2: "fas fa-map-signs",
     description:
       "Data Science and AI workloads are ubiquitous. From taking care of our health, to running businesses, to managing our energy systems and transport planning, we leverage data science and learning to make more informed decisions. We are able to obtain these insights through the combination of smart algorithms and access to vast amounts of data. Data management now poses a bottleneck that can unfortunately slow down the entire pipeline. The way data is stored and accessed strongly influences how fast the algorithms can provide us with useful insights.",
     direction:
       "The needs of Data Science workloads are poorly met by current general-purpose storage systems. Consequently, to obtain fast results systems rely on heuristics, approximations, or use of stale information. I am interested in designing and building a new storage system that (1) scales with the vast datasets used by Data Science workloads, (2) ingests and cleans new incoming data at high throughput, (3) serves data with low latency, and (4) is energy-efficient. This challenging goal entails many research directions, such as identifying opportunities to reduce data movement, designing adaptable data structures that harmonize with Data Science workloads, and the creative use of new storage resources (e.g., NVRAM, fast SSDs, etc.).",
     keywords: ["Data Science", "Big Data", "Energy Effiency"],
     id: "research1",
-    image_url: "/static/deck_images/new_white_data_science.png",
   },
   {
-    title: "Reimagining Storage Building Blocks for Fast Devices",
+    title: "Storage Building Blocks for Fast Devices",
+    subtitle1: "Context",
+    subtitle2: "Research Direction",
+    icon1: "fas fa-globe",
+    icon2: "fas fa-map-signs",
     description:
       "Emerging storage technologies challenge fundamental assumptions in computer systems design. One major assumption being challenged is the significant performance gap between memory and persistent storage access. This gap is now bridged by Byte-addressable persistent memory (e.g., Intel 3D XPoint). Another assumption is that I/O bandwidth is the main bottleneck in storage systems. This too has changed with the development of new fast drives (e.g., Intel Optane NVMe SSDs) shifting the bottleneck to the CPU. In addition, the storage stack is getting deeper and more heterogeneous. It is likely that in a typical server developers and system administrators will have to manage RAM, persistent memory, different types of SSDs and hard disks.",
     direction:
@@ -21,6 +29,10 @@ export const ResearchItems = [
   },
   {
     title: "Data Management for the Internet of Things",
+    subtitle1: "Context",
+    subtitle2: "Research Direction",
+    icon1: "fas fa-globe",
+    icon2: "fas fa-map-signs",
     description:
       "The Internet of Things (IoT) is a fast growing field which produces vast amounts of data. In fact, it is estimated that the data produced by IoT workloads alone in 2025 will be larger than all of the data we will produce in 2020. Naturally, this is an excellent opportunity for storage research.",
     direction:
