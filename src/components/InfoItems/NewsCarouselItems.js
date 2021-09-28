2f92c19003493514ed9f91f9745842635430a794
System Administrator <root@Oanas-MacBook-Pro.local>
2021-09-28 10:26:29 -0400

    group picture

--- src/components/InfoItems/NewsCarouselItems.js
+++ src/components/InfoItems/NewsCarouselItems.js
@@ -2,6 +2,9 @@
 // Stored as array of objects, where each object corresponds to a slide in the news carousel
 export const NewsCarouselItems = [
   {
+    image_url: "/static/carousel_images/group-picture.png",
+  },
+  {
     image_url: "/static/carousel_images/mcgill.jpeg",
   },
   {
