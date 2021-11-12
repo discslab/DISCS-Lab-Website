42f87252ba92d377fe33d8111763ad7915a73da1
System Administrator <root@Oanas-MacBook-Pro.local>
2021-11-12 15:44:39 -0500

    started the seminar page

--- src/components/Seminar/Seminar.js
+++ src/components/Seminar/Seminar.js
@@ -11,10 +11,22 @@ class Seminar extends Component {
       <section className="seminar" id="seminar">
         <Jumbotron fluid>
           <Container fluid>
-            <h1 className="jumbo-title">Systems Seminar</h1>
+            <h1 className="jumbo-title">McGill CS Systems Seminar</h1>
           </Container>
         </Jumbotron>
+        <div className="seminar-wrapper">
+          <div className="seminar-section">
+
+            <div className="seminar-description">
+              Roughly bi-weekly, the McGill CS systems group is running a seminar, which is open for McGill students and invited speakers to present their latest work. If you would like to present, propose a speaker, or join our mailing list, please get in touch at oana.balmau@cs.mcgill.ca
+              <br />
+              <br />
+              Past seminars:
+            </div>
+          </div>
+        </div>
       </section>
+
     );
   }
 }
