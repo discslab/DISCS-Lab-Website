1509750adbc828aea5a2a4b78f1e45d3f645aa77
System Administrator <root@Oanas-MacBook-Pro.local>
2021-11-12 16:01:33 -0500

    seminar page still ..

--- src/components/Seminar/Seminar.js
+++ src/components/Seminar/Seminar.js
@@ -14,11 +14,11 @@ class Seminar extends Component {
             <h1 className="jumbo-title">McGill CS Systems Seminar</h1>
           </Container>
         </Jumbotron>
-        <div className="openings-wrapper">
-          <div className="openings-section">
-            <span className="openings-subtitle">Upcoming Seminars</span>
+        <div className="seminar-wrapper">
+          <div className="seminar-section">
+            <span className="seminar-subtitle">Upcoming Seminars</span>
 
-            <div className="openings-current-description">
+            <div className="seminar-current-description">
               Roughly bi-weekly, the McGill CS systems group is running a seminar, 
               which is open for McGill students and invited speakers to present their 
               latest work. If you would like to present, propose a speaker, or join 
