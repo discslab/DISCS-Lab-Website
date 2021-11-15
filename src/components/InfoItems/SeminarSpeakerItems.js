095076b4373874b8d8bc359f27cb40d89942c318
System Administrator <root@Oanas-MacBook-Pro.local>
2021-11-15 15:54:17 -0500

    added all seminar speakers

--- src/components/InfoItems/SeminarSpeakerItems.js
+++ src/components/InfoItems/SeminarSpeakerItems.js
@@ -16,5 +16,52 @@ export const Speakers = [
     slides: "https://drive.google.com/file/d/1fOTyBYxZnXArYmQRBp7ICKAyuWl-MUNq/view"
   },
 
+  {
+    date: "Sep 30, 2021",
+    name: "Xinran Xiong",
+    talk: "Learned Indexes: A Study of Memory Consumption",
+    slides: "https://drive.google.com/file/d/1ebJT58g4LiW-hzCpT1dwg2U33FXxRLYP/view"
+  },
+
+    {
+    date: "Sep 16, 2021",
+    name: "Naums Mogers",
+    talk: "Mapping Parallelism in Lift through Constraint Satisfaction",
+    slides: "https://drive.google.com/file/d/1tHaR7cdEKd8ELfDdw-vved5dUkrS02Z4/view"
+  },
+
+  {
+    date: "Jul 22, 2021",
+    name: "Olamilekan Fadahunsi",
+    talk: "Task Scheduling Algorithms for Network Edge Applications",
+    slides: "https://drive.google.com/file/d/1Y9w1bgB8930Hw61A9-IJQH7Ez8AZcv7x/view"
+  },
+    {
+    date: "Jul 8, 2021",
+    name: "Hanfeng Chen",
+    talk: "HorsePower: an Optimization Framework for Data Analytics",
+    slides: "https://drive.google.com/file/d/1RAvmsqyOTbnBlTt8bwHpGi5TlJDQC5ok/view"
+  },
+
+  {
+    date: "Jun 17, 2021",
+    name: "Richard Olaniyan",
+    talk: "Synchronization Schemes for IoT and Edge Applications",
+    slides: "https://drive.google.com/file/d/1FK0H1CDca5hNbHu9gDezPyhDn5etwN7W/view"
+  },
+
+    {
+    date: "May 27, 2021",
+    name: "Omar Asad",
+    talk: "Dynamic Object Partitioning for Cooperative Cache",
+    slides: ""
+  },
+
+  {
+    date: "May 13, 2021",
+    name: "Yong Zhang",
+    talk: "Optimizing Learned Indexes for Write-Intensive Workloads",
+    slides: "https://drive.google.com/file/d/1PxAWzzGqprrp1gtqRiyZGgHeIPKX_KjI/view"
+  },
 
 ];
